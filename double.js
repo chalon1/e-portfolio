33a6f3ce50a0ac8d6ae0788923817bc584230119
uwase chalon <uwasechalon1@gmail.com>
2026-02-21 16:38:24 +0200

    New changes after 10 months

--- double.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const letters = document.querySelectorAll(".myName span");
-    letters.forEach((letter, index) => {
-        letter.style.animationDelay = `${index * 2}s`;
-    });
-});
